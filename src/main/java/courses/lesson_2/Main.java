01699c9ed18ee6c6fa1f520fe3cfd416d1ef1fac
ElenaBuzhan <elena.buzhan@zigzag.global>
2024-03-22 21:15:16 +0300

    lesson_2 - refactor code

--- src/main/java/courses/lesson_2/Main.java
+++ src/main/java/courses/lesson_2/Main.java
@@ -8,42 +8,44 @@ public class Main {
         printColor();
         compareNumbers();
     }
-    public static void printThreeWords(){
+
+    public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
 
     public static void checkSumSign() {
-        int a=-1;
-        int b=1;
-        int c=a+b;
-        if (c>=0) {
+        int a = -1;
+        int b = 1;
+        int c = a + b;
+        if (c >= 0) {
             System.out.println("Сумма положительная");
         } else {
             System.out.println("Сумма отрицательная");
         }
     }
+
     public static void printColor() {
-        int value=100;
-        if (value<=0) {
+        int value = 100;
+        if (value <= 0) {
             System.out.println("Красный");
-        } else if (value<=100) {
+        } else if (value <= 100) {
             System.out.println("Желтый");
         } else {
             System.out.println("Зеленый");
         }
 
-}
+    }
 
     public static void compareNumbers() {
-        int a=10;
-        int b=11;
-        if (a>=b) {
+        int a = 10;
+        int b = 11;
+        if (a >= b) {
             System.out.println("a >= b");
         } else {
             System.out.println("a < b");
         }
-}
+    }
 
 }
