439aae2a3ac5baee4ab543dd7d69804467979de8
ElenaBuzhan <elena.buzhan@zigzag.global>
2024-12-16 20:21:05 +0300

    lesson 18 - create allure report

--- src/test/java/courses/lesson_16/PostmanApiTest.java
+++ src/test/java/courses/lesson_16/PostmanApiTest.java
@@ -1,5 +1,7 @@
 package courses.lesson_16;
 
+import io.qameta.allure.Step;
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
@@ -14,36 +16,48 @@ public class PostmanApiTest {
     private static final String BASE_URI = "http://postman-echo.com";
     private static final Integer STATUS_CODE_OK = 200;
 
+    private io.restassured.specification.RequestSpecification baseRequest() {
+        return given()
+                .baseUri(BASE_URI)
+                .filter(new AllureRestAssured())
+                .log().uri();
+    }
+
     @Test
     public void testGetMethod() {
-        given()
-                .baseUri(BASE_URI)
+        performGetRequest();
+    }
+
+    @Step("Выполнение GET-запроса с параметрами foo1=bar1 и foo2=bar2")
+    private void performGetRequest() {
+        baseRequest()
                 .when()
                 .get("/get?foo1=bar1&foo2=bar2")
                 .then()
-                .assertThat()
                 .statusCode(STATUS_CODE_OK)
                 .body("args.foo1", equalTo("bar1"))
                 .body("args.foo2", equalTo("bar2"))
-                .body("url", equalTo(BASE_URI + "/get?foo1=bar1&foo2=bar2"))
-                .log().all();
+                .body("url", equalTo(BASE_URI + "/get?foo1=bar1&foo2=bar2"));
     }
 
     @Test
     public void testPostRawTextMethod() {
         HashMap<String, String> body = new HashMap<>();
         body.put("test", "value");
-        given()
-                .baseUri(BASE_URI)
+        performPostRawRequest(body);
+    }
+
+    @Step("Выполнение POST-запроса с JSON-данными: {body}")
+    private void performPostRawRequest(HashMap<String, String> body) {
+        baseRequest()
                 .contentType(ContentType.JSON)
-                .when()
                 .body(body)
+                .when()
                 .post("/post")
                 .then()
                 .statusCode(STATUS_CODE_OK)
-                .body("data.test", equalTo("value"))
-                .body("url", equalTo(BASE_URI + "/post"))
-                .log().all();
+                .body("data.test", equalTo(body.get("test")))
+                .body("url", equalTo(BASE_URI + "/post"));
     }
 
     @Test
@@ -51,68 +65,80 @@ public class PostmanApiTest {
         HashMap<String, String> form = new HashMap<>();
         form.put("foo1", "bar1");
         form.put("foo2", "bar2");
-        given()
-                .baseUri(BASE_URI)
+        performPostFormRequest(form);
+    }
+
+    @Step("Выполнение POST-запроса с form-data: {form}")
+    private void performPostFormRequest(HashMap<String, String> form) {
+        baseRequest()
                 .contentType("application/x-www-form-urlencoded;charset=UTF-8")
                 .formParams(form)
                 .when()
                 .post("/post")
                 .then()
                 .statusCode(STATUS_CODE_OK)
-                .body("form.foo1", equalTo("bar1"))
-                .body("form.foo2", equalTo("bar2"))
-                .body("url", equalTo(BASE_URI + "/post"))
-                .log().all();
+                .body("form.foo1", equalTo(form.get("foo1")))
+                .body("form.foo2", equalTo(form.get("foo2")))
+                .body("url", equalTo(BASE_URI + "/post"));
     }
 
     @Test
     public void testPutMethod() {
-        HashMap<String, String> test = new HashMap<>();
-        test.put("Name", "Lena");
-        given()
-                .baseUri(BASE_URI)
+        HashMap<String, String> data = new HashMap<>();
+        data.put("Name", "Lena");
+        performPutRequest(data);
+    }
+
+    @Step("Выполнение PUT-запроса с JSON-данными: {data}")
+    private void performPutRequest(HashMap<String, String> data) {
+        baseRequest()
                 .contentType(ContentType.JSON)
+                .body(data)
                 .when()
-                .body(test)
                 .put("/put")
                 .then()
                 .statusCode(STATUS_CODE_OK)
-                .body("data.Name", equalTo("Lena"))
-                .body("url", equalTo(BASE_URI + "/put"))
-                .log().all();
+                .body("data.Name", equalTo(data.get("Name")))
+                .body("url", equalTo(BASE_URI + "/put"));
     }
 
     @Test
     public void testPatchMethod() {
-        HashMap<String, String> test = new HashMap<>();
-        test.put("Name", "Elena");
-        given()
-                .baseUri(BASE_URI)
+        HashMap<String, String> data = new HashMap<>();
+        data.put("Name", "Elena");
+        performPatchRequest(data);
+    }
+
+    @Step("Выполнение PATCH-запроса с JSON-данными: {data}")
+    private void performPatchRequest(HashMap<String, String> data) {
+        baseRequest()
                 .contentType(ContentType.JSON)
+                .body(data)
                 .when()
-                .body(test)
                 .patch("/patch")
                 .then()
                 .statusCode(STATUS_CODE_OK)
-                .body("data.Name", equalTo("Elena"))
-                .body("url", equalTo(BASE_URI + "/patch"))
-                .log().all();
+                .body("data.Name", equalTo(data.get("Name")))
+                .body("url", equalTo(BASE_URI + "/patch"));
     }
 
     @Test
     public void testDeleteMethod() {
-        HashMap<String, String> test = new HashMap<>();
-        test.put("Name", "Lena");
-        given()
-                .baseUri(BASE_URI)
+        HashMap<String, String> data = new HashMap<>();
+        data.put("Name", "Lena");
+        performDeleteRequest(data);
+    }
+
+    @Step("Выполнение DELETE-запроса с JSON-данными: {data}")
+    private void performDeleteRequest(HashMap<String, String> data) {
+        baseRequest()
                 .contentType(ContentType.JSON)
+                .body(data)
                 .when()
-                .body(test)
                 .delete("/delete")
                 .then()
                 .statusCode(STATUS_CODE_OK)
-                .body("data.Name", equalTo("Lena"))
-                .body("url", equalTo(BASE_URI + "/delete"))
-                .log().all();
+                .body("data.Name", equalTo(data.get("Name")))
+                .body("url", equalTo(BASE_URI + "/delete"));
     }
 }
